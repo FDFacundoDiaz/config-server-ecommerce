bb7bebcd6105af7b9bb911724c7aaf19def918e9
unknown <facundo.dz226@gmail.com>
2024-11-26 16:04:36 -0300

    eliminacion de servicios de autenticacion y validacion de usuarios con jwt en base de datos

--- springsecurity/src/main/java/com/springsecurity/repository/IRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springsecurity.repository;
-import com.springsecurity.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IRoleRepository extends JpaRepository<Role, Long> {
-}
